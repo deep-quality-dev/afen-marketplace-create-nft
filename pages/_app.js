7b003b19baff165eabefd113933c45c8cbe67ded
deep-quality-dev <hungrywarrior081@gmail.com>
2021-06-27 22:12:13 +0400

    fix: import file name typo

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { UserProvider } from "../components/User";
 import { ContractProvider } from "../components/Contract";
 import { NotificationProvider } from "../components/Notification";
 import "../styles/globals.css";
-import Layout from "../components/layout/";
+import Layout from "../components/Layout/";
 
 function App({ Component, pageProps }) {
   return (
