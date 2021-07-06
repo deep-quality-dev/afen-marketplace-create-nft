d5f5f28a922a80e6173da3bafce5080780b7ba4c
deep-quality-dev <hungrywarrior081@gmail.com>
2021-07-06 19:03:09 +0400

    chore: add padding to nft image on larger screens

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -103,7 +103,7 @@ export default function Token({ nft }: NFTPageProps) {
     <div className="w-screen md:h-screen overflow-hidden">
       <div className="flex flex-wrap lg:flex-nowrap justify-between">
         <div className="mb-5 lg:mb-0 w-full mt-10 py-10 md:mt-0 md:py-0 md:h-screen md:w-3/5 lg:w-4/6 pt-16 flex flex-col items-center justify-between bg-gray-100 dark:bg-gray-500">
-          <div className="h-96 px-8 md:px-0 md:h-5/6 w-full md:w-5/6 my-auto">
+          <div className="h-96 px-8 md:px-0 md:h-5/6 md:pt-10 w-full md:w-5/6 my-auto">
             <div className="relative h-full w-full">
               <Image
                 loading="eager"
